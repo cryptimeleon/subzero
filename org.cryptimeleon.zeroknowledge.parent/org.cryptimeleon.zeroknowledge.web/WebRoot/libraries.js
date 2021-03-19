97d52f94bfc989de16388f6cd9abd165845aa2ab
Jeremy Fell <jfell@sfu.ca>
2021-03-19 13:14:16 -0700

    Updated xtext and jquery js libraries versions

--- org.cryptimeleon.zeroknowledge.parent/org.cryptimeleon.zeroknowledge.web/WebRoot/libraries.js
+++ org.cryptimeleon.zeroknowledge.parent/org.cryptimeleon.zeroknowledge.web/WebRoot/libraries.js
@@ -11,9 +11,9 @@ if (fileIndex > 0) {
 require.config({
   baseUrl: baseUrl,
   paths: {
-    "jquery": "webjars/jquery/3.3.1-1/jquery.min",
+    "jquery": "webjars/jquery/3.5.1/jquery.min",
     "ace/ext/language_tools": "webjars/ace/1.3.3/src/ext-language_tools",
-    "xtext/xtext-ace": "xtext/2.17.0/xtext-ace"
+    "xtext/xtext-ace": "xtext/2.24.0/xtext-ace"
   }
 });
 require(["webjars/ace/1.3.3/src/ace"], function() {
