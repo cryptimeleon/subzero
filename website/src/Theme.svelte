4b247a8e58a0dd28869e94da2a34f9453412f598
Jeremy Fell <jfell@sfu.ca>
2021-08-12 16:21:17 -0700

    Fixed issue where warnings/errors would not be underlined in editor code

--- website/src/Theme.svelte
+++ website/src/Theme.svelte
@@ -17,4 +17,24 @@
 		margin: 0;
 		overflow: hidden;
 	}
+
+    // Fixes issue where syntax/semantic errors are not underlined in red
+    // CSS is from xtext-web source code
+    :global(.xtext-marker_error) {
+        position: absolute;
+        z-index: 30;
+        background-image: url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAQAAAADCAYAAAC09K7GAAAABmJLR0QA/wD/AP+gvaeTAAAAHElEQVQI12NggIL/DAz/GdA5/xkY/qPKMDAwAADLZwf5rvm+LQAAAABJRU5ErkJggg==");
+        background-repeat: repeat-x;
+        background-position: left bottom;
+    }
+
+    // Fixes issue where syntax/semantic warnings are not underlined in yellow
+    // CSS is from xtext-web source code
+    :global(.xtext-marker_warning) {
+        position: absolute;
+        z-index: 20;
+        background-image: url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAQAAAADCAYAAAC09K7GAAAABmJLR0QA/wD/AP+gvaeTAAAAMklEQVQI12NkgIIvJ3QXMjAwdDN+OaEbysDA4MPAwNDNwMCwiOHLCd1zX07o6kBVGQEAKBANtobskNMAAAAASUVORK5CYII=");
+        background-repeat: repeat-x;
+        background-position: left bottom;
+    }
 </style>
