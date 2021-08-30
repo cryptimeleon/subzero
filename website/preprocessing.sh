ffde54d955e7c687aca9dd527a22095bf2ce071d
Jeremy Fell <jfell@sfu.ca>
2021-08-30 14:48:45 -0700

    Added include tag for README generation to allow content for website only

--- website/preprocessing.sh
+++ website/preprocessing.sh
@@ -4,7 +4,14 @@ sed -i "s/'\//'/g" index.html
 
 # Copy GitHub README and remove sections that should be excluded from website documentation
 sed '/^<!--startExclude-->/,/^<!--endExclude-->/d' README.md > temp.md
+
+# Uncomment sections that should be included only in website documentation
+sed -i 's/<!--startInclude//g' temp.md
+sed -i 's/endInclude-->//g' temp.md
+
+# Escape all backticks
 sed -i 's/`/\\`/g' temp.md
+
 printf '`' >> temp.md
 printf '// This file is generated automatically. Do not make any changes to this file, as they will be overwritten\nexport default `\n' | cat - temp.md > src/documentation.js
 rm temp.md
