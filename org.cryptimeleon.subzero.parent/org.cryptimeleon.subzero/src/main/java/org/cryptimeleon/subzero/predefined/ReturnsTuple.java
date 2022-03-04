2b761fe8e230ba805c7f3edbbdec2097dc9c63e3
Jeremy Fell <jeremyafell@gmail.com>
2022-03-03 23:42:39 -0800

    Refactor ReturnsTuples annotation from Xtend to Java

--- org.cryptimeleon.subzero.parent/org.cryptimeleon.subzero/src/main/java/org/cryptimeleon/subzero/predefined/ReturnsTuple.java
+++ org.cryptimeleon.subzero.parent/org.cryptimeleon.subzero/src/main/java/org/cryptimeleon/subzero/predefined/ReturnsTuple.java
@@ -1,22 +1,25 @@
-package org.cryptimeleon.subzero.predefined
+package org.cryptimeleon.subzero.predefined;
 
-import java.lang.annotation.Target
-import java.lang.annotation.Retention
+import java.lang.annotation.Target;
+import java.lang.annotation.Retention;
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 
 /**
  * Use this annotation on methods in the PredefinedFunctions class
  * to specify that the method returns a tuple, and what the multiplicity
  * of the tuple is.
- * 
+ *
  * This allows the validator to validate tuple multiplicities in operations
- * 
+ *
  * Usage example:
  * @ReturnsTuple(x)
  * where x is the multiplicity of the tuple returned by the function
  */
 @Retention(RUNTIME)
 @Target(METHOD)
-annotation ReturnsTuple {
-	int value = 1
+public @interface ReturnsTuple {
+    int value() default 1;
 }
