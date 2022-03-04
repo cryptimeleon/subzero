28f66dbf609de4c4f5643c618c6d789d123f05b7
Jeremy Fell <jeremyafell@gmail.com>
2022-03-03 22:10:00 -0800

    Change ProjectFile to cache files after reading them

--- org.cryptimeleon.subzero.parent/org.cryptimeleon.subzero/src/main/java/org/cryptimeleon/subzero/project/ProjectFile.java
+++ org.cryptimeleon.subzero.parent/org.cryptimeleon.subzero/src/main/java/org/cryptimeleon/subzero/project/ProjectFile.java
@@ -3,6 +3,8 @@ package org.cryptimeleon.subzero.project;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Represents a file in the generated project.
@@ -10,7 +12,9 @@ import java.util.Base64;
  * the file can be loaded (from text or binary)
  */
 public class ProjectFile {
-	
+
+	private static final Map<String, String> fileCache = new HashMap<>();
+
 	private final String name;
 	private String contents;
 	
@@ -18,18 +22,29 @@ public class ProjectFile {
 		this.name = name;
 		this.contents = contents;
 	}
-	
+
+	// A ProjectFile should not be constructed for a file whose contents can change,
+	// as the contents of the file are only read once and then cached
 	public ProjectFile(String name, String partialPath, boolean isBinary) {
 		this.name = name;
 		String path = "/" + partialPath + '/' + name;
+
+		contents = fileCache.get(path);
+		if (contents != null) {
+			return;
+		}
 		
 		try {
 			byte[] fileContent = ProjectFile.class.getResourceAsStream(path).readAllBytes();
+
 			if (isBinary) {		
 				contents = Base64.getEncoder().encodeToString(fileContent);
 			} else {
 				contents = new String(fileContent, StandardCharsets.UTF_8);
 			}
+
+			fileCache.put(path, contents);
+
 		} catch (NullPointerException | IOException e) {
 			System.err.println("Cannot read project file: " + path);
 		}
