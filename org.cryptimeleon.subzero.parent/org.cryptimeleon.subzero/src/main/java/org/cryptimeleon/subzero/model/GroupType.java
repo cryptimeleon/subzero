6909e6eb628ebc1785ab1921e3522b26edeb8a2b
Jeremy Fell <jeremyafell@gmail.com>
2022-03-04 16:48:08 -0800

    Simplify GroupType toString method

--- org.cryptimeleon.subzero.parent/org.cryptimeleon.subzero/src/main/java/org/cryptimeleon/subzero/model/GroupType.java
+++ org.cryptimeleon.subzero.parent/org.cryptimeleon.subzero/src/main/java/org/cryptimeleon/subzero/model/GroupType.java
@@ -7,12 +7,7 @@ public enum GroupType {
 	UNKNOWN; // Used when a group element is assigned multiple groups, which will create a validation error
 	
 	public String toString() {
-		switch(this) {
-			case G1: return "G1";
-			case G2: return "G2";
-			case GT: return "GT";
-			case UNKNOWN: return "unknown";
-			default: return "";
-		}
+		if (this == UNKNOWN) return "unknown";
+		return name();
 	}
 }
